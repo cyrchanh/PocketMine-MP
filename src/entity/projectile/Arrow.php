e11fccad22438169615533074f87fd6e22c47f53
Gummy Bear <101925404+cyrchanh@users.noreply.github.com>
2026-04-21 17:23:14 +0100

    Add pierceLevel handling to Arrow entity

--- src/entity/projectile/Arrow.php
+++ src/entity/projectile/Arrow.php
@@ -80,6 +80,7 @@ class Arrow extends Projectile{
 
 		$this->pickupMode = $nbt->getByte(self::TAG_PICKUP, self::PICKUP_ANY);
 		$this->critical = $nbt->getByte(self::TAG_CRIT, 0) === 1;
+		$this->pierceLevel = $nbt->getByte("PierceLevel", 0);
 		$this->collideTicks = $nbt->getShort(self::TAG_LIFE, $this->collideTicks);
 	}
 
@@ -87,6 +88,7 @@ class Arrow extends Projectile{
 		$nbt = parent::saveNBT();
 		$nbt->setByte(self::TAG_PICKUP, $this->pickupMode);
 		$nbt->setByte(self::TAG_CRIT, $this->critical ? 1 : 0);
+		$nbt->setByte("PierceLevel", $this->pierceLevel);
 		$nbt->setShort(self::TAG_LIFE, $this->collideTicks);
 		return $nbt;
 	}
@@ -162,62 +164,60 @@ class Arrow extends Projectile{
 		$this->broadcastSound(new ArrowHitSound());
 	}
 
-	public function onHitBlock(Block $blockHit, RayTraceResult $hitResult): void
-	    {
-	        parent::onHitBlock($blockHit, $hitResult);
-	
-	        // PIERCING: reset pierce tracking when arrow embeds in a block
-	        $this->pierceLevel = 0;
-	        $this->resetPiercedEntities();
-	
-	        $this->broadcastSound(new ArrowHitSound());
-	        $this->collideTicks = 0;
-	    }
+	public function onHitBlock(Block $blockHit, RayTraceResult $hitResult): void{
+		parent::onHitBlock($blockHit, $hitResult);
 
-	public function onHitEntity(Entity $entityHit, RayTraceResult $hitResult): void
-		{
-	        if ($this->hasPiercedEntity($entityHit->getId())) {
-	            return;
-	        }
-	
-	        parent::onHitEntity($entityHit, $hitResult);
-	
-	        $damage = $this->getResultDamage();
-	
-	        if ($damage >= 0) {
-	            if ($this->critical) {
-	                $damage += mt_rand(0, (int) ($damage / 2) + 1);
-	            }
-	
-	            $ev = new EntityDamageByEntityEvent($this->getOwningEntity() ?? $this, $entityHit, EntityDamageEvent::CAUSE_PROJECTILE, $damage);
-	
-	            $entityHit->attack($ev);
-	
-	            if ($this->punchKnockback > 0) {
-	                $horizontalSpeed = sqrt($this->motion->x ** 2 + $this->motion->z ** 2);
-	                if ($horizontalSpeed > 0) {
-	                    $multiplier = $this->punchKnockback * 0.6 / $horizontalSpeed;
-	                    $entityHit->setMotion($entityHit->getMotion()->add($this->motion->x * $multiplier, 0.1, $this->motion->z * $multiplier));
-	                }
-	            }
-	
-	            if ($this->isOnFire()) {
-	                $entityHit->setOnFire(5);
-	            }
-	
-	            if ($this->pierceLevel > 0) {
-	                $this->piercedEntityIds[] = $entityHit->getId();
-	
-	                if (count($this->piercedEntityIds) > $this->pierceLevel) {
-	                    $this->flagForDespawn();
-	                }
-	            } else {
-	                $this->flagForDespawn();
-	            }
-	
-	            $this->broadcastSound(new ArrowHitSound());
-	        }
-	    }
+		// PIERCING: reset pierce tracking when arrow embeds in a block
+		$this->pierceLevel = 0;
+		$this->resetPiercedEntities();
+
+		$this->broadcastSound(new ArrowHitSound());
+		$this->collideTicks = 0;
+	}
+
+	public function onHitEntity(Entity $entityHit, RayTraceResult $hitResult): void{
+		if ($this->hasPiercedEntity($entityHit->getId())) {
+			return;
+		}
+
+		parent::onHitEntity($entityHit, $hitResult);
+
+		$damage = $this->getResultDamage();
+
+		if ($damage >= 0) {
+			if ($this->critical) {
+				$damage += mt_rand(0, (int) ($damage / 2) + 1);
+			}
+
+			$ev = new EntityDamageByEntityEvent($this->getOwningEntity() ?? $this, $entityHit, EntityDamageEvent::CAUSE_PROJECTILE, $damage);
+
+			$entityHit->attack($ev);
+
+			if ($this->punchKnockback > 0) {
+				$horizontalSpeed = sqrt($this->motion->x ** 2 + $this->motion->z ** 2);
+				if ($horizontalSpeed > 0) {
+					$multiplier = $this->punchKnockback * 0.6 / $horizontalSpeed;
+					$entityHit->setMotion($entityHit->getMotion()->add($this->motion->x * $multiplier, 0.1, $this->motion->z * $multiplier));
+				}
+			}
+
+			if ($this->isOnFire()) {
+				$entityHit->setOnFire(5);
+			}
+
+			if ($this->pierceLevel > 0) {
+				$this->piercedEntityIds[] = $entityHit->getId();
+
+				if (count($this->piercedEntityIds) > $this->pierceLevel) {
+					$this->flagForDespawn();
+				}
+			} else {
+				$this->flagForDespawn();
+			}
+
+			$this->broadcastSound(new ArrowHitSound());
+		}
+	}
 
 	public function getPickupMode() : int{
 		return $this->pickupMode;
